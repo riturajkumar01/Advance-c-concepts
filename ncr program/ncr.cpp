0e28d1ac7cd8862896bd254aef08aa9f9bfa0153
riturajkumar01 <rk4940@srmist.edu.in>
2024-07-02 23:01:43 +0530

    ncr program

---
+++ ncr program/ncr.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+
+    int factorial = 1;
+
+    for(int i =1 ;i<=n;i++){
+    factorial = factorial*i;
+}
+return factorial;
+}
+int nCr(int n, int r){
+    int num = factorial(n);
+    int denom = factorial(r) * factorial (n-r);
+    return num/denom;
+    
+}
+int main(){
+    int n , r;
+    cin >> n >> r;
+    cout<<"Answer is "<<nCr(n, r) << endl;
+    
+}
